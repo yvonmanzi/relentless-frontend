a6a298912f0c823824ca43945bb57865da90b8a7
yvon <yvonmanzi@gmail.com>
2024-07-24 13:19:41 +0200

    Login Form

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import LoginForm from "./components/LoginForm";
-
+//TODO: Will use routes here
 function App() {
   return <LoginForm />;
 }
--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,9 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
+
+import login from "../authService.js";
 
 const LoginForm = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
+
+  const handleLogin = async (event) => {
+    event.preventDefault();
+    try {
+      const response = await login(username, password);
+      // Some redirection
+      console.log("Login Successful");
+    } catch (e) {
+      setError(error);
+      console.error("Login Failed", error);
+    }
+  };
   return (
     <div className="max-w-sm mx-auto p-6 bg-white rounded-lg shadow-md">
-      <form>
+      <form onSubmit={handleLogin}>
         <div className="mb-4">
           <label
             className="block text-gray-700 text-sm font-bold mb-2"
@@ -16,6 +33,7 @@ const LoginForm = () => {
             id="email"
             type="email"
             placeholder="Email"
+            onChange={(event) => setUsername(event.target.value)}
           />
         </div>
         <div className="mb-6">
@@ -30,18 +48,20 @@ const LoginForm = () => {
             id="password"
             type="password"
             placeholder="Password"
+            onChange={(event) => setPassword(event.target.value)}
           />
         </div>
         <div className="flex items-center justify-between">
           <button
             className="bg-gray-900 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-            type="button"
+            type="submit"
+            onSubmit={handleLogin}
           >
             Sign In
           </button>
           <a
             className="inline-block align-baseline font-bold text-sm text-gray-500 hover:text-gray-800"
-            href="#"
+            href=""
           >
             Forgot password?
           </a>
